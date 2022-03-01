53ec07da01ecf2fa2817c96016e590913de56599
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-03-01 14:31:08 -0500

    Initial Commit

---
+++ mapbox-swiftui-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mapbox-swiftui-test
+//
+//  Created by Adrian Vatchinsky on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mapbox-swiftui-test/mapbox_swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapbox_swiftui_testApp.swift
+//  mapbox-swiftui-test
+//
+//  Created by Adrian Vatchinsky on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct mapbox_swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
